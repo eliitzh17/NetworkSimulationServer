31d9f0f6db20dac5d2970d216a63e68ab88ef855
eliitzh17 <32660616+eliitzh17@users.noreply.github.com>
2025-05-26 07:56:09 +0300

    fix deployments

--- main_publish_simulations_worker.py
+++
@@ -1,8 +0,0 @@
-import functions_framework
-from app.workers.outbox_publishers_workers.publish_simulations_worker import main as worker_main
-
-@functions_framework.http
-def publish_simulations_worker(request):
-    # TODO: Parse event/message from request if needed
-    worker_main()  # Should be refactored to process a single event
-    return "Completed", 200 
--- run_all_workers.sh
+++ run_all_workers.sh
@@ -2,15 +2,16 @@
 
 # Deploy all worker functions as Google Cloud Functions
 
-gcloud functions deploy consumer-simulations-worker \
+gcloud functions deploy consumer_simulations_worker \
   --set-env-vars=PYTHONPATH="/app",MONGODB_URI="mongodb+srv://eliitzh17:AX3lGegN341ozKF2@cluster0.qp71kbn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",MONGODB_DB="network_sim_db",RABBITMQ_URL="amqps://zzwmeqgv:CBqAUi7xOh0u8hj8mt9IDmpcQrJofi1H@horse.lmq.cloudamqp.com/zzwmeqgv" \
   --runtime python310 \
   --trigger-http \
   --allow-unauthenticated \
-  --entry-point consumer_simulations_worker \
-  --memory 512Mi \
-  --region us-central1 \
-  --source .
+  --entry-point=consumer_simulations_worker \
+  --memory=512Mi \
+  --region=us-central1 \
+  --source=.
+
 
 gcloud functions deploy consumer-links-worker \
   --set-env-vars=PYTHONPATH="/app",MONGODB_URI="mongodb+srv://eliitzh17:AX3lGegN341ozKF2@cluster0.qp71kbn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",MONGODB_DB="network_sim_db",RABBITMQ_URL="amqps://zzwmeqgv:CBqAUi7xOh0u8hj8mt9IDmpcQrJofi1H@horse.lmq.cloudamqp.com/zzwmeqgv" \
